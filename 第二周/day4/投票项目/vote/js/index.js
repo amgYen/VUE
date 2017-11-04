0e412494d3a417a96c6ef3365b07591f8d18180a
zhufengpeixun <1144709265@qq.com>
2017-11-04 18:28:36 +0800

    day4

---
+++ 第二周/day4/投票项目/vote/js/index.js
@@ -0,0 +1,10 @@
+﻿// new Dialog("在首页这测试测试",function(){
+//     console.log(1);
+// })
+// let obj = {"id":1,"name":"lily"};
+// cookie.set("userInfo",JSON.stringify(obj));
+// //console.log(JSON.parse(cookie.get("userInfo")));
+// cookie.remove("userInfo");
+
+
+
